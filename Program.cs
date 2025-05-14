e43139f2ad7b304d28706989ad54a2e20cf14aa1
Seongyoon-LEE <ssyy9727@naver.com>
2025-05-14 10:13:39 +0900

    CSharp 코드
    콘솔에서 출력하기및 문제풀기

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+//namespace _04_18_CSharp
+//{
+//    internal class Program
+//    {
+//        ////static : 미리 메모리 할당 되는것
+//        static void Main(string[] args)
+//        {        // 메인 함수(메서드)
+//                 // 프로그램 시작점 Entry point
+//                 // 한프로젝트의 하나만 존재
+
+//            /*Console.WriteLine("MBC\n아카데미! "); // cw tab + tab
+//                                               //클래스명.함수 
+//            int num1 = 20;   // int : 정수형식 자료형 4byte
+//            int num2 = 30;
+//            int sum = num1 + num2;
+//            Console.WriteLine(sum); // 출력 */
+
+//            Console.WriteLine("정수를 입력 바람 \n");
+//            int n1 = int.Parse(Console.ReadLine()!);
+//            Console.WriteLine("입력한 정수는 : " + n1 + "이다.");
+//            n1++; // 1 증가 
+//            Console.WriteLine("증가된 정수는 : " + n1 + "이다");
+//            n1--; // 1 감소
+//            Console.WriteLine("증가된 정수는 : " + n1 + "이다");
+
+//        }
+//    }
+//}
